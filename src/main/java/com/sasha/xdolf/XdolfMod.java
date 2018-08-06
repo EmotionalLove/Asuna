67f4ad2b1d599dcf653a4d400206065874511774
2F4Uof2b2t <totsuka.sama@gmail.com>
2018-08-06 13:33:24 -0700

    Initial commit

---
+++ src/main/java/com/sasha/xdolf/XdolfMod.java
@@ -0,0 +1,28 @@
+package com.sasha.xdolf;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = XdolfMod.MODID, name = XdolfMod.NAME, version = XdolfMod.VERSION)
+public class XdolfMod
+{
+    public static final String MODID = "xdolfforge";
+    public static final String NAME = "Xdolf";
+    public static final String VERSION = "4.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
