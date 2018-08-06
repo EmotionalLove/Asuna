22c18a79b9a9025b9d947dd899d22098cc094977
2F4Uof2b2t <totsuka.sama@gmail.com>
2018-08-06 14:02:51 -0700

    Begin working with mixins.

--- src/main/java/com/sasha/xdolf/XdolfMod.java
+++ src/main/java/com/sasha/xdolf/XdolfMod.java
@@ -8,13 +8,12 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = XdolfMod.MODID, name = XdolfMod.NAME, version = XdolfMod.VERSION)
-public class XdolfMod
-{
+public class XdolfMod {
     public static final String MODID = "xdolfforge";
     public static final String NAME = "Xdolf";
     public static final String VERSION = "4.0";
 
-    private static Logger logger;
+    public static Logger logger;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
@@ -23,6 +22,10 @@ public class XdolfMod
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/sasha/xdolf/mixins/MixinLoaderForge.java
+++ src/main/java/com/sasha/xdolf/mixins/MixinLoaderForge.java
@@ -1,7 +1,46 @@
 package com.sasha.xdolf.mixins;
 
+import com.sasha.xdolf.XdolfMod;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.mixin.Mixins;
+
+import javax.annotation.Nullable;
+import java.util.Map;
+
 /**
  * Created by Sasha on 06/08/2018 at 1:34 PM
  **/
-public class MixinLoaderForge {
+public class MixinLoaderForge implements IFMLLoadingPlugin {
+    protected static boolean isObfuscatedEnvironment;
+    @Override
+    public String[] getASMTransformerClass() {
+        XdolfMod.logger.info("Xdolf is initialising mixins.");
+        MixinBootstrap.init();
+        Mixins.addConfiguration("mixins.kami.json");
+        MixinEnvironment.getDefaultEnvironment().setObfuscationContext("searge");
+        XdolfMod.logger.info("Obfuscation -> " + MixinEnvironment.getDefaultEnvironment().getObfuscationContext());
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+        isObfuscatedEnvironment = (boolean) (Boolean) data.get("runtimeDeobfuscationEnabled");
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
 }
