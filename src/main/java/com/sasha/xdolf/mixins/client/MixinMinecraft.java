25460724b84a0eb88045fe755302fbd1fabfcdbd
2F4Uof2b2t <totsuka.sama@gmail.com>
2018-08-07 18:27:10 -0700

    client mixins

--- src/main/java/com/sasha/xdolf/mixins/client/MixinMinecraft.java
+++ src/main/java/com/sasha/xdolf/mixins/client/MixinMinecraft.java
@@ -1,6 +1,7 @@
-package com.sasha.xdolf.mixins.server;
+package com.sasha.xdolf.mixins.client;
 
 import com.sasha.xdolf.XdolfMod;
+import net.minecraft.client.Minecraft;
 import net.minecraft.server.MinecraftServer;
 import org.apache.logging.log4j.Level;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,8 +12,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 /**
  * Created by Sasha on 06/08/2018 at 9:48 PM
  **/
-@Mixin(MinecraftServer.class)
-public class MixinMinecraftServer {
+@Mixin(Minecraft.class)
+public class MixinMinecraft {
 
     @Inject(method = "initialWorldChunkLoad", at = @At("HEAD"), cancellable = true)
     public void initialWorldChunkLoad(CallbackInfo callbackInfo){
