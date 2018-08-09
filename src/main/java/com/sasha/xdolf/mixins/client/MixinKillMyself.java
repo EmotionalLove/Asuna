6d7b946298559b9626c649094ad96742f6c94d18
2F4Uof2b2t <totsuka.sama@gmail.com>
2018-08-09 14:54:24 -0700

    WHOA WAIT GuiIngameForge????

--- src/main/java/com/sasha/xdolf/mixins/client/MixinKillMyself.java
+++ src/main/java/com/sasha/xdolf/mixins/client/MixinKillMyself.java
@@ -4,18 +4,18 @@ import com.sasha.xdolf.XdolfMod;
 import com.sasha.xdolf.events.ClientOverlayRenderEvent;
 import com.sasha.xdolf.gui.XdolfHUD;
 import net.minecraft.client.gui.GuiIngame;
+import net.minecraftforge.client.GuiIngameForge;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(GuiIngame.class)
+@Mixin(GuiIngameForge.class)
 public class MixinKillMyself{
     @Inject(method = "renderGameOverlay", at = @At("RETURN"))
     public void renderGameOverlay(float partialTicks, CallbackInfo info){
-        XdolfHUD.renderScreen();
+        //XdolfHUD.renderScreen();
         ClientOverlayRenderEvent ev = new ClientOverlayRenderEvent(partialTicks);
         XdolfMod.EVENT_MANAGER.invokeEvent(ev);
-
     }
 }
