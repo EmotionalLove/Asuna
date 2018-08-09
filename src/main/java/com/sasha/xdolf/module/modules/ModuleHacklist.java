bf5da30c03ae6f38a6c26a47d3240696a185efa1
2F4Uof2b2t <totsuka.sama@gmail.com>
2018-08-09 07:26:06 -0700

    sasha u the big dumb

---
+++ src/main/java/com/sasha/xdolf/module/modules/ModuleHacklist.java
@@ -0,0 +1,21 @@
+package com.sasha.xdolf.module.modules;
+
+import com.sasha.xdolf.XdolfMod;
+import com.sasha.xdolf.module.XdolfCategory;
+import com.sasha.xdolf.module.XdolfModule;
+
+/**
+ * Created by Sasha on 08/08/2018 at 7:51 PM
+ **/
+public class ModuleHacklist extends XdolfModule {
+    public ModuleHacklist() {
+        super("Hacklist", XdolfCategory.GUI, true);
+    }
+
+    @Override
+    public void onTick(){
+        if (XdolfMod.mc.currentScreen != null){
+
+        }
+    }
+}
