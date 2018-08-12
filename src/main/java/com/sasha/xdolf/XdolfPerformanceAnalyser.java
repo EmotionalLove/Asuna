bb7515ca0e3cd4af62b7b55b64b79fce5ba0961e
2F4Uof2b2t <totsuka.sama@gmail.com>
2018-08-12 11:25:04 -0700

    fml i can never make normal arrays work sometimes lol

--- src/main/java/com/sasha/xdolf/XdolfPerformanceAnalyser.java
+++ src/main/java/com/sasha/xdolf/XdolfPerformanceAnalyser.java
@@ -1,54 +1,56 @@
 package com.sasha.xdolf;
 
+import java.util.ArrayList;
+
 /**
  * Created by Sasha on 11/08/2018 at 6:36 PM
  **/
 public class XdolfPerformanceAnalyser {
 
-    private int[] performanceTimesNormal;
-    private int[] performanceTimesRender;
+    private ArrayList<Integer> performanceTimesNormal;
+    private ArrayList<Integer> performanceTimesRender;
     private int counterNormal=0;
     private int counterRender=0;
 
     public XdolfPerformanceAnalyser(){
-        performanceTimesNormal = new int[]{};
-        performanceTimesRender = new int[]{};
+        performanceTimesNormal = new ArrayList<>();
+        performanceTimesRender = new ArrayList<>();
     }
 
     public void recordNewNormalTime(int totalTime) {
         if (counterNormal > 1200) {
             counterNormal = 0;
         }
-        performanceTimesNormal[counterNormal]=totalTime;
+        performanceTimesNormal.set(counterNormal, totalTime);
         counterNormal++;
     }
     public void recordNewRenderTime(int totalTime) {
         if (counterRender > 1200){
             counterRender =0;
         }
-        performanceTimesRender[counterRender]=totalTime;
+        performanceTimesRender.set(counterRender,totalTime);
         counterRender++;
     }
 
     public int getCurrentMsNormal(){
-        return performanceTimesNormal[counterNormal == 0 ? counterNormal : counterNormal-1];
+        return performanceTimesNormal.get(counterNormal == 0 ? counterNormal : counterNormal-1);
     }
     public double getAvgMsNormal(){
         int sum=0;
         for (int i : performanceTimesNormal) {
             sum+=i;
         }
-        return sum/performanceTimesNormal.length;
+        return sum/performanceTimesNormal.size();
     }
     public int getCurrentMsRender(){
-        return performanceTimesRender[counterRender == 0 ? counterRender : counterRender-1];
+        return performanceTimesRender.get(counterRender == 0 ? counterRender : counterRender-1);
     }
     public double getAvgMsRender(){
         int sum=0;
         for (int i : performanceTimesRender) {
             sum+=i;
         }
-        return sum/performanceTimesRender.length;
+        return sum/performanceTimesRender.size();
     }
 
 }
