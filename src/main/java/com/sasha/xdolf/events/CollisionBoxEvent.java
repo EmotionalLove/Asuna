e4d9ec0b71a0129e0fedfae4d264984ea276dbb9
2F4Uof2b2t <totsuka.sama@gmail.com>
2018-08-12 15:22:36 -0700

    jeezuz u hoe

---
+++ src/main/java/com/sasha/xdolf/events/CollisionBoxEvent.java
@@ -0,0 +1,44 @@
+package com.sasha.xdolf.events;
+
+import com.sasha.eventsys.SimpleEvent;
+import net.minecraft.block.Block;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+
+/**
+ * Created by Sasha on 12/08/2018 at 2:58 PM
+ **/
+public class CollisionBoxEvent extends SimpleEvent {
+    private Block block;
+    private BlockPos pos;
+    private AxisAlignedBB aabb;
+
+    public CollisionBoxEvent(Block block, BlockPos pos, AxisAlignedBB aabb) {
+        this.block = block;
+        this.pos = pos;
+        this.aabb = aabb;
+    }
+    public Block getBlock() {
+        return block;
+    }
+
+    public BlockPos getPos() {
+        return pos;
+    }
+
+    public AxisAlignedBB getAabb() {
+        return aabb;
+    }
+
+    public void setAabb(AxisAlignedBB aabb) {
+        this.aabb = aabb;
+    }
+
+    public void setBlock(Block block) {
+        this.block = block;
+    }
+
+    public void setPos(BlockPos pos) {
+        this.pos = pos;
+    }
+}
---
+++ src/main/java/com/sasha/xdolf/module/modules/ModuleJesus.java
@@ -0,0 +1,77 @@
+package com.sasha.xdolf.module.modules;
+
+import com.sasha.eventsys.SimpleEventHandler;
+import com.sasha.eventsys.SimpleListener;
+import com.sasha.xdolf.XdolfMod;
+import com.sasha.xdolf.events.ClientPacketRecieveEvent;
+import com.sasha.xdolf.events.ClientPacketSendEvent;
+import com.sasha.xdolf.events.CollisionBoxEvent;
+import com.sasha.xdolf.module.XdolfCategory;
+import com.sasha.xdolf.module.XdolfModule;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockLiquid;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
+import net.minecraft.network.play.client.CPacketPlayer;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+
+/**
+ * Created by Sasha on 12/08/2018 at 2:48 PM
+ **/
+public class ModuleJesus extends XdolfModule implements SimpleListener {
+    public ModuleJesus() {
+        super("Jesus", XdolfCategory.MOVEMENT, false);
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void onTick() {
+
+    }
+    @SimpleEventHandler
+    public void onPacketTx(ClientPacketSendEvent e) {
+        if (e.getSendPacket() instanceof CPacketPlayer) {
+            if (isOverWater(XdolfMod.minecraft.player) && !XdolfMod.minecraft.gameSettings.keyBindSneak.isPressed()) {
+                int t = XdolfMod.minecraft.player.ticksExisted % 2;
+                if (t == 0) {
+                    ((CPacketPlayer) e.getSendPacket()).y += 0.02;
+                }
+            }
+        }
+    }
+    @SimpleEventHandler
+    public void onBoxFormed(CollisionBoxEvent e) {
+        if (!(e.getBlock() instanceof BlockLiquid)) {
+            return;
+        }
+        if (XdolfMod.minecraft.player.isInWater() || XdolfMod.minecraft.gameSettings.keyBindSneak.isKeyDown() || XdolfMod.minecraft.player.fallDistance > 2) {
+            return;
+        }
+        e.setAabb(new AxisAlignedBB(0, 0, 0, 1, 0.99, 1));
+    }
+    private boolean isOverWater(Entity e) {
+        BlockPos pos = e.getPosition();
+        IBlockState state = XdolfMod.minecraft.world.getBlockState(pos.add(0, -1, 0));
+        if (state.getBlock() == Blocks.WATER || state.getBlock() == Blocks.LAVA) {
+            return !XdolfMod.minecraft.player.isInWater() && state.getBlock() != Blocks.FLOWING_WATER && state.getBlock() != Blocks.FLOWING_LAVA;
+        }
+        return false;
+    }
+
+    private boolean inLiquid(Entity e) {
+        double Y = e.getPosition().getY() + 0.1;
+        Block block = XdolfMod.minecraft.world.getBlockState(new BlockPos(e.getPosition().getX(), Y, e.getPosition().getZ())).getBlock();
+        return block instanceof BlockLiquid && block != Blocks.FLOWING_WATER && block != Blocks.FLOWING_LAVA;
+    }
+}
