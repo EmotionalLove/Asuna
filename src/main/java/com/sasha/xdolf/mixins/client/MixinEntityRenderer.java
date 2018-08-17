b8d20961f57d7b0b46732925153d0f3f988b179e
EmotionalLove <totsuka.sama@gmail.com>
2018-08-17 12:04:56 -0700

    Some gay elytra stuff.

--- src/main/java/com/sasha/xdolf/mixins/client/MixinEntityRenderer.java
+++ src/main/java/com/sasha/xdolf/mixins/client/MixinEntityRenderer.java
@@ -5,6 +5,7 @@ import com.sasha.xdolf.module.modules.ModuleTracers;
 import net.minecraft.client.renderer.EntityRenderer;
 import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,14 +16,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(EntityRenderer.class)
 public class MixinEntityRenderer {
 
-
-    @Inject(method = "orientCamera", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/util/math/Vec3d;distanceTo(Lnet/minecraft/util/math/Vec3d;)D"), cancellable = true)
+    /*
+    @Inject(method = "orientCamera", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;rotate(FFFF)V", ordinal = 3), cancellable = true)
     public void orientCamera(float partialTicks, CallbackInfo info) {
         if (ModuleManager.getModuleByName("CameraClip").isEnabled()) {
+            double d3 = (double)(this.thirdPersonDistancePrev + (4.0F - this.thirdPersonDistancePrev) * partialTicks);
             info.cancel();
         }
     }
-
+*/
     @Inject(method = "renderWorldPass", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/EntityRenderer;renderHand(FI)V"))
     public void renderWorldPass(int pass, float partialTicks, long finishTimeNano, CallbackInfo info){
         ModuleManager.renderModules();
