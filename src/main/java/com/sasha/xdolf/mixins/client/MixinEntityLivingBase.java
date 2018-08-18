d410c1f3cf01c2c27a8eb7e8b35a3831724a5ea4
EmotionalLove <totsuka.sama@gmail.com>
2018-08-18 13:13:52 -0700

    Implement lowjump

--- src/main/java/com/sasha/xdolf/mixins/client/MixinEntityLivingBase.java
+++ src/main/java/com/sasha/xdolf/mixins/client/MixinEntityLivingBase.java
@@ -1,17 +1,16 @@
 package com.sasha.xdolf.mixins.client;
 
+import com.sasha.xdolf.XdolfMod;
 import com.sasha.xdolf.events.PlayerJumpEvent;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.init.MobEffects;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
-import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import javax.annotation.Nullable;
 
@@ -25,7 +24,11 @@ public abstract class MixinEntityLivingBase {
      * @author Sasha Stevens
      * @reason bc ZOZZLE ofc
      */
-    @Inject(method = "jump", at = @At("HEAD"), cancellable = true)
-    protected void jump(CallbackInfo info){
+    @Inject(method = "getJumpUpwardsMotion", at = @At("HEAD"), cancellable = true)
+    protected void getJumpUpwardsMotion(CallbackInfoReturnable<Float> info) {
+        PlayerJumpEvent event = new PlayerJumpEvent(0.42f);
+        XdolfMod.EVENT_MANAGER.invokeEvent(event);
+        info.setReturnValue(event.getJumpHeight());
+        info.cancel();
     }
 }
