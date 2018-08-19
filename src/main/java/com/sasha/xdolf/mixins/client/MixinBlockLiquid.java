db2b2d083b61c49d6bbe801a94712c96dbd5c412
EmotionalLove <totsuka.sama@gmail.com>
2018-08-18 18:36:37 -0700

    fix jeezus

--- src/main/java/com/sasha/xdolf/mixins/client/MixinBlockLiquid.java
+++ src/main/java/com/sasha/xdolf/mixins/client/MixinBlockLiquid.java
@@ -1,7 +1,6 @@
 package com.sasha.xdolf.mixins.client;
 
-import com.sasha.xdolf.XdolfMod;
-import com.sasha.xdolf.events.MinecraftGetAABBEvent;
+import com.sasha.xdolf.module.modules.ModuleJesus;
 import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.AxisAlignedBB;
@@ -10,18 +9,16 @@ import net.minecraft.world.IBlockAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static com.sasha.xdolf.module.modules.ModuleJesus.WATER_AABB;
 import static net.minecraft.block.Block.NULL_AABB;
 
 @Mixin(BlockLiquid.class)
 public class MixinBlockLiquid {
     @Inject(method = "getCollisionBoundingBox", at = @At("HEAD"), cancellable = true)
     public void getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos, CallbackInfoReturnable<AxisAlignedBB> info) {
-        MinecraftGetAABBEvent event = new MinecraftGetAABBEvent(NULL_AABB);
-        XdolfMod.EVENT_MANAGER.invokeEvent(event);
-        info.setReturnValue(event.getAxisAlignedBB());
+        info.setReturnValue(ModuleJesus.INSTANCE.doJesus() ? WATER_AABB : NULL_AABB);
         info.cancel();
     }
 }
