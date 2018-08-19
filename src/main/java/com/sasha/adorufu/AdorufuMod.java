428bd52b04bd2d1dd6cc14d2b133c8708efe6f8a
EmotionalLove <totsuka.sama@gmail.com>
2018-08-19 15:13:42 -0700

    im rly sad now ;-; sry x0

--- src/main/java/com/sasha/adorufu/AdorufuMod.java
+++ src/main/java/com/sasha/adorufu/AdorufuMod.java
@@ -38,7 +38,7 @@ import java.util.concurrent.TimeUnit;
 public class AdorufuMod {
     public static final String MODID = "adorufuforge";
     public static final String NAME = "Adorufu";
-    public static final String JAP_NAME = "アドルフ";
+    public static final String JAP_NAME = "\u30A2\u30C9\u30EB\u30D5";
     public static final String VERSION = "1.0pre";
 
     private static Logger logger = LogManager.getLogger("Adorufu " + VERSION);
