f0ec9c4127fde600c00667bade00617da101eee3
EmotionalLove <totsuka.sama@gmail.com>
2018-08-18 20:24:14 -0700

    announcer

--- src/main/java/com/sasha/xdolf/module/modules/ModuleAnnouncer.java
+++ src/main/java/com/sasha/xdolf/module/modules/ModuleAnnouncer.java
@@ -24,10 +24,8 @@ public class ModuleAnnouncer extends XdolfModule implements SimpleListener {
     static boolean swap = false;
     public static int counter = 0;
 
-    private ArrayList<String> blocksPlacedStr = new ArrayList<>();
-    private ArrayList<Integer> blocksPlacedInt = new ArrayList<>();
-    private ArrayList<String> blocksBrokenStr = new ArrayList<>();
-    private ArrayList<Integer> blocksBrokenInt = new ArrayList<>();
+    private LinkedHashMap<String, Integer> blocksBrokenMap = new LinkedHashMap<>();
+    private LinkedHashMap<String, Integer> blocksPlacedMap = new LinkedHashMap<>();
 
 
 
@@ -49,60 +47,58 @@ public class ModuleAnnouncer extends XdolfModule implements SimpleListener {
         if (!this.isEnabled()) return;
         counter++;
         if (counter > 20*30) {
-            //logMsg(false, "Refreshing announcer");
             Random rand = new Random();
-            int swap = rand.nextInt(10);
-            if (swap > 5){
-                if (blocksBrokenStr.isEmpty()) {
-                    counter = 0;
+            boolean randBool = rand.nextBoolean();
+            if (randBool) {
+                if (blocksBrokenMap.isEmpty()) {
                     return;
                 }
-                XdolfMod.minecraft.player.sendChatMessage("> I just mined " + blocksBrokenInt.get(0) + " " + blocksBrokenStr.get(0));
-                blocksBrokenStr.remove(0);
-                blocksBrokenInt.remove(0);
+                String key = "";
+                for (Map.Entry<String, Integer> stringIntegerEntry : blocksBrokenMap.entrySet()) {
+                    key = stringIntegerEntry.getKey();
+                    XdolfMod.minecraft.player.sendChatMessage("> I just mined " + stringIntegerEntry.getKey() + " " +
+                            stringIntegerEntry.getValue());
+                    break;
+                }
+                blocksBrokenMap.remove(key);
             }
             else {
-                if (blocksPlacedStr.isEmpty()) {
-                    counter = 0;
+                if (blocksPlacedMap.isEmpty()) {
                     return;
                 }
-                XdolfMod.minecraft.player.sendChatMessage("> I just placed " + blocksPlacedInt.get(0) + " " + blocksPlacedStr.get(0));
-                blocksPlacedInt.remove(0);
-                blocksPlacedStr.remove(0);
+                String key = "";
+                for (Map.Entry<String, Integer> stringIntegerEntry : blocksPlacedMap.entrySet()) {
+                    key = stringIntegerEntry.getKey();
+                    XdolfMod.minecraft.player.sendChatMessage("> I just mined " + stringIntegerEntry.getKey() + " " +
+                            stringIntegerEntry.getValue());
+                    break;
+                }
+                blocksPlacedMap.remove(key);
             }
-            counter = 0;
         }
 
     }
     @SimpleEventHandler
     public void onBlockBreak(PlayerBlockBreakEvent e){
         //logMsg("ok");
-        if (blocksBrokenStr.contains(e.getBlock().getLocalizedName())){
+        if (blocksBrokenMap.containsKey(e.getBlock().getLocalizedName())){
             //logMsg("ok 1");
-            blocksBrokenInt.set(blocksBrokenStr.indexOf(e.getBlock().getLocalizedName()),
-                    blocksPlacedInt.get(blocksBrokenStr.indexOf(e.getBlock().getLocalizedName()))+1);
-            logMsg(blocksBrokenStr.get(0));
+            blocksBrokenMap.put(e.getBlock().getLocalizedName(), blocksBrokenMap.get(e.getBlock().getLocalizedName())+1);
             return;
         }
         //logMsg("oh ok");
-        blocksBrokenStr.add(e.getBlock().getLocalizedName());
-        blocksBrokenInt.add(1);
-        logMsg(blocksBrokenStr.get(0));
+        blocksBrokenMap.put(e.getBlock().getLocalizedName(), 1);
     }
     @SimpleEventHandler
     public void onBlockBreak(PlayerBlockPlaceEvent e){
         //logMsg("ok");
-        if (blocksPlacedStr.contains(e.getBlock().getLocalizedName())){
+        if (blocksPlacedMap.containsKey(e.getBlock().getLocalizedName())){
             //logMsg("ok 1");
-            blocksPlacedInt.set(blocksPlacedStr.indexOf(e.getBlock().getLocalizedName()),
-                    blocksPlacedInt.get(blocksPlacedStr.indexOf(e.getBlock().getLocalizedName()))+1);
-            //logMsg(blocksPlacedStr.get(0));
+            blocksPlacedMap.put(e.getBlock().getLocalizedName(), blocksPlacedMap.get(e.getBlock().getLocalizedName())+1);
             return;
         }
-        logMsg("oh ok");
-        blocksPlacedStr.add(e.getBlock().getLocalizedName());
-        blocksPlacedInt.add(1);
-        logMsg(blocksPlacedStr.get(0));
+        //logMsg("oh ok");
+        blocksPlacedMap.put(e.getBlock().getLocalizedName(), 1);
     }
 
 }
