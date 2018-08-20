cdea8d5cd09d7015042f98cd5dd9174cc34bdbe3
EmotionalLove <totsuka.sama@gmail.com>
2018-08-19 17:53:27 -0700

    smh

---
+++ src/main/java/com/sasha/adorufu/mixins/client/MixinAbstractClientPlayer.java
@@ -0,0 +1,19 @@
+package com.sasha.adorufu.mixins.client;
+
+import com.sasha.adorufu.module.ModuleManager;
+import net.minecraft.client.entity.AbstractClientPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AbstractClientPlayer.class)
+public class MixinAbstractClientPlayer {
+    @Inject(method = "isSpectator", at = @At("HEAD"), cancellable = true)
+    public void isSpectator(CallbackInfoReturnable<Boolean> info) {
+        if (ModuleManager.getModuleByName("Freecam").isEnabled()) {
+            info.setReturnValue(true);
+            info.cancel();
+        }
+    }
+}
