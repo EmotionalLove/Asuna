66bf84fc16539f198dad294c4094e877d10f3454
EmotionalLove <totsuka.sama@gmail.com>
2018-08-21 19:30:23 -0700

    WolfIdentity

---
+++ src/main/java/com/sasha/adorufu/module/modules/ModuleWolfIdentity.java
@@ -0,0 +1,50 @@
+package com.sasha.adorufu.module.modules;
+
+import com.sasha.adorufu.AdorufuMod;
+import com.sasha.adorufu.module.AdorufuCategory;
+import com.sasha.adorufu.module.AdorufuModule;
+import com.sasha.adorufu.module.ModuleInfo;
+import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.EntityTameable;
+
+@ModuleInfo(description = "Show who tamed a wolf")
+public class ModuleWolfIdentity extends AdorufuModule {
+
+    public ModuleWolfIdentity() {
+        super("WolfIdentity", AdorufuCategory.RENDER, false);
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void onTick() {
+        if (this.isEnabled()) {
+            for (Entity entity : AdorufuMod.minecraft.world.getLoadedEntityList()) {
+                if (entity instanceof EntityTameable) {
+                    EntityTameable tameableEntity = (EntityTameable) entity;
+                    if (tameableEntity.isTamed()) {
+                        EntityRenderer.drawNameplate(AdorufuMod.minecraft.fontRenderer, tameableEntity.getOwnerId().toString(),
+                                (float)tameableEntity.posX,
+                                (float)tameableEntity.posY + 1.5f,
+                                (float)tameableEntity.posZ,
+                                0,
+                                AdorufuMod.minecraft.player.rotationYaw,
+                                AdorufuMod.minecraft.player.rotationPitch,
+                                false,
+                                false
+                                );
+                    }
+                }
+            }
+        }
+    }
+}
