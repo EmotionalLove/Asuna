93ce18a57800054dae83e5e3a214a4d7f2e26d2a
EmotionalLove <totsuka.sama@gmail.com>
2018-08-26 11:34:03 -0700

    finally

--- src/main/java/com/sasha/adorufu/events/ServerGenerateChunkEvent.java
+++ src/main/java/com/sasha/adorufu/events/ServerGenerateChunkEvent.java
@@ -8,10 +8,12 @@ public class ServerGenerateChunkEvent extends SimpleEvent {
     private int chunkX;
     private int chunkZ;
     private Chunk chunk;
+    //private SPacketChunkData packetIn;
 
     public ServerGenerateChunkEvent(Chunk chunk, int chunkX, int chunkY) {
         this.chunkX = chunkX;
         this.chunkZ = chunkY;
+        this.chunk = chunk;
     }
 
     public int getChunkX() {
