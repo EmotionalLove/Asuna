1acaf0ae2e83df080de4cebfe832ec8b1b51ee66
EmotionalLove <totsuka.sama@gmail.com>
2018-08-29 06:14:22 -0700

    meme

--- src/main/java/com/sasha/adorufu/module/modules/ModuleNoSlow.java
+++ src/main/java/com/sasha/adorufu/module/modules/ModuleNoSlow.java
@@ -1,15 +1,17 @@
 package com.sasha.adorufu.module.modules;
 
-import com.sasha.adorufu.AdorufuMod;
-import com.sasha.adorufu.module.ModuleInfo;
+import com.sasha.adorufu.events.ClientSlowDownPlayerEvent;
 import com.sasha.adorufu.module.AdorufuCategory;
 import com.sasha.adorufu.module.AdorufuModule;
+import com.sasha.adorufu.module.ModuleInfo;
+import com.sasha.eventsys.SimpleEventHandler;
+import com.sasha.eventsys.SimpleListener;
 
 /**
  * Created by Sasha on 11/08/2018 at 3:18 PM
  **/
 @ModuleInfo(description = "Don't slow down whilst eating.") //todo
-public class ModuleNoSlow extends AdorufuModule {
+public class ModuleNoSlow extends AdorufuModule implements SimpleListener {
     public ModuleNoSlow() {
         super("NoSlow", AdorufuCategory.MOVEMENT, false);
     }
@@ -26,9 +28,11 @@ public class ModuleNoSlow extends AdorufuModule {
 
     @Override
     public void onTick() {
-        if (AdorufuMod.minecraft.player.isHandActive()) {
-            AdorufuMod.minecraft.player.moveForward *= 2;
-            AdorufuMod.minecraft.player.moveStrafing *= 2;
-        }
+
+    }
+
+    @SimpleEventHandler
+    public void onSlowDown(ClientSlowDownPlayerEvent e) {
+        if (this.isEnabled()) e.setCancelled(true);
     }
 }
