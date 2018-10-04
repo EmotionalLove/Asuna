5a6591e07714d1551dbdc86a906f6c92e5fd0805
Sasha <sashwho@vivaldi.net>
2018-10-04 11:41:58 -0700

    Try to fix a reconnecct issue.

--- src/main/java/com/sasha/adorufu/mod/mixins/client/MixinNetworkManager.java
+++ src/main/java/com/sasha/adorufu/mod/mixins/client/MixinNetworkManager.java
@@ -28,13 +28,15 @@ import io.netty.util.concurrent.GenericFutureListener;
 import net.minecraft.network.INetHandler;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
+import net.minecraft.network.ThreadQuickExitException;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import javax.annotation.Nullable;
+import java.util.Queue;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Created by Sasha on 08/08/2018 at 8:08 PM
@@ -42,34 +44,95 @@ import javax.annotation.Nullable;
 @Mixin(value = NetworkManager.class, priority = 999)
 public abstract class MixinNetworkManager {
 
-    @Shadow protected abstract void dispatchPacket(Packet<?> inPacket, @Nullable GenericFutureListener<? extends Future<? super Void>>[] futureListeners);
+    @Shadow
+    protected abstract void dispatchPacket(Packet<?> inPacket, @Nullable GenericFutureListener<? extends Future<? super Void>>[] futureListeners);
 
-    @Shadow public INetHandler packetListener;
+    @Shadow
+    public INetHandler packetListener;
 
-    @Shadow public Channel channel;
+    @Shadow
+    public Channel channel;
 
-    @Inject(method = "sendPacket(Lnet/minecraft/network/Packet;)V",
+    @Shadow
+    public abstract boolean isChannelOpen();
+
+    @Shadow
+    protected abstract void flushOutboundQueue();
+
+    @Shadow
+    @Final
+    public Queue<NetworkManager.InboundHandlerTuplePacketListener> outboundPacketsQueue;
+
+    @Shadow
+    @Final
+    public ReentrantReadWriteLock readWriteLock;
+
+    /**
+     * @author
+     */
+    @Overwrite
+    public void sendPacket(Packet<?> packetIn) {
+        if (this.isChannelOpen()) {
+            ClientPacketSendEvent event = new ClientPacketSendEvent(packetIn);
+            AdorufuMod.EVENT_MANAGER.invokeEvent(event);
+            if (event.isCancelled()) return;
+            this.flushOutboundQueue();
+            this.dispatchPacket(event.getSendPacket(), (GenericFutureListener[]) null);
+        } else {
+            this.readWriteLock.writeLock().lock();
+
+            try {
+                this.outboundPacketsQueue.add(new NetworkManager.InboundHandlerTuplePacketListener(packetIn, new GenericFutureListener[0]));
+            } finally {
+                this.readWriteLock.writeLock().unlock();
+            }
+        }
+    }
+
+    /*@Inject(method = "sendPacket(Lnet/minecraft/network/Packet;)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetworkManager;flushOutboundQueue()V"), cancellable = true)
-    public void sendPacket(Packet<?> packetIn, CallbackInfo info){
+    public void sendPacket(Packet<?> packetIn, CallbackInfo info) {
         ClientPacketSendEvent event = new ClientPacketSendEvent(packetIn);
         AdorufuMod.EVENT_MANAGER.invokeEvent(event);
-        if (event.isCancelled()){
+        if (event.isCancelled()) {
             info.cancel();
             return;
         }
         this.dispatchPacket(event.getSendPacket(), null);
         info.cancel();
+    }*/
+
+    /**
+     * @author
+     */
+    @Overwrite
+    protected void channelRead0(ChannelHandlerContext p_channelRead0_1_, Packet<?> p_channelRead0_2_) throws Exception
+    {
+        if (this.channel.isOpen())
+        {
+            try
+            {
+                ClientPacketRecieveEvent event = new ClientPacketRecieveEvent(p_channelRead0_2_);
+                AdorufuMod.EVENT_MANAGER.invokeEvent(event);
+                ((Packet<INetHandler>)event.getRecievedPacket()).processPacket(this.packetListener);
+            }
+            catch (ThreadQuickExitException ignored)
+            {
+                //
+            }
+        }
     }
+/*
     @Inject(method = "channelRead0",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/network/Packet;processPacket(Lnet/minecraft/network/INetHandler;)V"), cancellable = true)
     protected void channelRead0(ChannelHandlerContext p_channelRead0_1_, Packet<?> p_channelRead0_2_, CallbackInfo info) throws Exception {
         ClientPacketRecieveEvent event = new ClientPacketRecieveEvent(p_channelRead0_2_);
         AdorufuMod.EVENT_MANAGER.invokeEvent(event);
-        if (event.isCancelled()){
+        if (event.isCancelled()) {
             info.cancel();
             return;
         }
-        ((Packet<INetHandler>)event.getRecievedPacket()).processPacket(this.packetListener);
+        ((Packet<INetHandler>) event.getRecievedPacket()).processPacket(this.packetListener);
         info.cancel();
-    }
+    }*/
 }
