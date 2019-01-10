ee1b972caf013aa8bbb4ace8bf37711597dd5c5d
EmotionalLove <sashwho@vivaldi.net>
2019-01-09 18:28:36 -0800

    Ensure invmove works in chests and other inventories

--- src/main/java/com/sasha/asuna/mod/mixin/client/MixinMovementInputFromOptions.java
+++ src/main/java/com/sasha/asuna/mod/mixin/client/MixinMovementInputFromOptions.java
@@ -3,7 +3,7 @@ package com.sasha.asuna.mod.mixin.client;
 import com.sasha.asuna.mod.AsunaMod;
 import com.sasha.asuna.mod.feature.impl.InventoryMoveFeature;
 import com.sasha.asuna.mod.misc.Manager;
-import net.minecraft.client.renderer.InventoryEffectRenderer;
+import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.util.MovementInput;
 import net.minecraft.util.MovementInputFromOptions;
@@ -17,7 +17,7 @@ public class MixinMovementInputFromOptions extends MovementInput {
 
     @Redirect(method = "updatePlayerMoveState", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/settings/KeyBinding;isKeyDown()Z"))
     public boolean isKeyPressed(KeyBinding keyBinding) {
-        if (Manager.Feature.isFeatureEnabled(InventoryMoveFeature.class) && AsunaMod.minecraft.currentScreen instanceof InventoryEffectRenderer) {
+        if (Manager.Feature.isFeatureEnabled(InventoryMoveFeature.class) && AsunaMod.minecraft.currentScreen instanceof GuiContainer) {
             return Keyboard.isKeyDown(keyBinding.getKeyCode());
         }
         return keyBinding.isKeyDown();
